522242a1e7d8c53c5ad671813d5527b0684cf536
Tim Gunnarsson <tim.awg@outlook.com>
2023-11-21 23:25:51 +0100

    Initial Commit

---
+++ Bambuser-Test/Bambuser_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bambuser_TestApp.swift
+//  Bambuser-Test
+//
+//  Created by Tim Gunnarsson on 2023-11-21.
+//
+
+import SwiftUI
+
+@main
+struct Bambuser_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
